1ff3ab7861a3ec0d6994d3f45b80eb6a01bdeae1
dongelen <dongelen@nhl.nl>
2016-07-04 18:45:11 +0200

    Roterende transformer

--- draw.js
+++ draw.js
@@ -19,15 +19,19 @@ var Rectangle = (function (_super) {
     function Rectangle() {
         _super.apply(this, arguments);
     }
-    Rectangle.prototype.drawOnCanvas = function (tansformer, canvas) {
-        var translatedPoint = tansformer.translateUpperLeft(this.myStartPoint, this.mySize);
-        var translatedSize = tansformer.translateSize(this.mySize);
+    Rectangle.prototype.drawOnCanvas = function (transformer, canvas) {
         var context = canvas.getContext("2d");
-        window.console.log(translatedPoint.x);
-        window.console.log(translatedPoint.y);
-        window.console.log(translatedSize.width);
-        window.console.log(translatedSize.height);
-        context.rect(translatedPoint.x, translatedPoint.y, translatedSize.width, translatedSize.height);
+        context.beginPath();
+        var leftBottom = transformer.translatePoint({ x: this.myStartPoint.x, y: this.myStartPoint.y });
+        var rightBottom = transformer.translatePoint({ x: this.myStartPoint.x + this.mySize.width, y: this.myStartPoint.y });
+        var rightTop = transformer.translatePoint({ x: this.myStartPoint.x + this.mySize.width, y: this.myStartPoint.y + this.mySize.height });
+        var leftTop = transformer.translatePoint({ x: this.myStartPoint.x, y: this.myStartPoint.y + this.mySize.height });
+        context.moveTo(leftBottom.x, leftBottom.y);
+        context.lineTo(rightBottom.x, rightBottom.y);
+        context.lineTo(rightTop.x, rightTop.y);
+        context.lineTo(leftTop.x, leftTop.y);
+        context.closePath();
+        // context.fill();
         context.stroke();
     };
     return Rectangle;
@@ -45,7 +49,6 @@ var Circle = (function (_super) {
         context.arc(translatedPoint.x, translatedPoint.y, translatedSize.width / 2, 0, 2 * Math.PI, false);
         context.fillStyle = 'green';
         context.fill();
-        context.lineWidth = 5;
         context.strokeStyle = '#003300';
         context.stroke();
     };
@@ -55,9 +58,9 @@ var Transformer = (function () {
     function Transformer() {
         this.canvasSize = null;
     }
-    Transformer.prototype.translateUpperLeft = function (p, s) {
+    Transformer.prototype.translatePoint = function (p) {
         var x = p.x * this.canvasSize.width;
-        var y = (1 - p.y - s.height) * this.canvasSize.height;
+        var y = (1 - p.y) * this.canvasSize.height;
         return { x: x, y: y };
     };
     Transformer.prototype.translateCenterPoint = function (p, s) {
@@ -78,9 +81,9 @@ var HorizontalMirrorTransformer = (function (_super) {
     function HorizontalMirrorTransformer() {
         _super.apply(this, arguments);
     }
-    HorizontalMirrorTransformer.prototype.translateUpperLeft = function (p, s) {
-        var x = (1 - p.x - s.width) * this.canvasSize.width;
-        var y = (1 - p.y - s.height) * this.canvasSize.height;
+    HorizontalMirrorTransformer.prototype.translatePoint = function (p) {
+        var x = (1 - p.x) * this.canvasSize.width;
+        var y = (1 - p.y) * this.canvasSize.height;
         return { x: x, y: y };
     };
     HorizontalMirrorTransformer.prototype.translateCenterPoint = function (p, s) {
@@ -90,9 +93,35 @@ var HorizontalMirrorTransformer = (function (_super) {
     };
     return HorizontalMirrorTransformer;
 }(Transformer));
+var RotateTransformer = (function (_super) {
+    __extends(RotateTransformer, _super);
+    function RotateTransformer(angleInDeg) {
+        if (angleInDeg === void 0) { angleInDeg = 10; }
+        _super.call(this);
+        this.angleInDeg = angleInDeg;
+        this.angleToRotateInDeg = 10;
+        this.angleToRotateInDeg = angleInDeg;
+    }
+    // Hoek uitrekenen tot boven scharnierpunt
+    // Lengte uitrekenen
+    // Nieuw punt uitrekenen aan de hand daarvan
+    RotateTransformer.prototype.translatePoint = function (p) {
+        var upperLeft = { x: -0.01, y: 1.01 };
+        var sideA = upperLeft.y - p.y;
+        var sideB = upperLeft.x + p.x;
+        var sideC = Math.sqrt(sideA * sideA + sideB * sideB);
+        var differenceInRad = this.angleToRotateInDeg / 180 * Math.PI;
+        var angleInRad = Math.atan(sideA / sideB);
+        var angleToRotateInRad = angleInRad + differenceInRad;
+        var sideANew = upperLeft.x + Math.cos(angleToRotateInRad) * sideC;
+        var sideBNew = upperLeft.y - Math.sin(angleToRotateInRad) * sideC;
+        window.console.log({ sideC: sideC, x: sideANew, y: sideBNew });
+        return _super.prototype.translatePoint.call(this, { x: sideANew, y: sideBNew });
+    };
+    return RotateTransformer;
+}(Transformer));
 var DrawArea = (function () {
     function DrawArea(canvas, transformer) {
-        if (transformer === void 0) { transformer = new Transformer(); }
         this.shapes = [];
         this.myCanvas = null;
         this.myCanvas = canvas;
@@ -110,22 +139,28 @@ var DrawArea = (function () {
     };
     return DrawArea;
 }());
-var r1 = new Rectangle({ x: 0, y: 0 }, { width: 1, height: 1 });
-var r2 = new Rectangle({ x: 0, y: 0 }, { width: 0.5, height: 0.5 });
-var r3 = new Rectangle({ x: 0.75, y: 0.75 }, { width: 0.1, height: 0.1 });
-var c1 = new Circle({ x: 0.5, y: 0.5 }, { width: 0.1, height: 0.1 });
+var r1 = new Rectangle({ x: 0.5, y: 0.5 }, { width: 0.2, height: 0.2 });
+var r2 = new Rectangle({ x: 0.3, y: 0.5 }, { width: 0.2, height: 0.2 });
+var r3 = new Rectangle({ x: 0.3, y: 0.8 }, { width: 0.2, height: 0.2 });
+var r4 = new Rectangle({ x: 0.5, y: 0.8 }, { width: 0.2, height: 0.2 });
+var r5 = new Rectangle({ x: 0.5, y: 0.9 }, { width: 0.2, height: 0.2 });
+var c1 = new Circle({ x: 0.1, y: 0.1 }, { width: 0.1, height: 0.1 });
 var canvas1 = document.getElementById("tekenvlak1");
 var canvas2 = document.getElementById("tekenvlak2");
-var drawArea1 = new DrawArea(canvas1);
-var drawArea2 = new DrawArea(canvas2, new HorizontalMirrorTransformer());
+var canvas3 = document.getElementById("tekenvlak3");
+var drawArea1 = new DrawArea(canvas1, new RotateTransformer());
+var drawArea2 = new DrawArea(canvas2, new Transformer());
+var drawArea3 = new DrawArea(canvas2, new RotateTransformer(-10));
 drawArea1.addShape(r1);
 drawArea1.addShape(r2);
 drawArea1.addShape(r3);
+drawArea1.addShape(r4);
+drawArea1.addShape(r5);
 drawArea1.addShape(c1);
-drawArea1.drawOnCanvas();
-// Mirror ding
 drawArea2.addShape(r1);
 drawArea2.addShape(r2);
-drawArea2.addShape(r3);
-drawArea2.addShape(c1);
+drawArea3.addShape(r1);
+drawArea3.addShape(r2);
+drawArea1.drawOnCanvas();
 drawArea2.drawOnCanvas();
+drawArea3.drawOnCanvas();
--- draw.ts
+++ draw.ts
@@ -23,20 +23,24 @@ class Shape {
 
 class Rectangle extends Shape {
 
-    drawOnCanvas (tansformer : Transformer, canvas : HTMLCanvasElement) {
-        let translatedPoint = tansformer.translateUpperLeft (this.myStartPoint, this.mySize);
-        let translatedSize = tansformer.translateSize (this.mySize);
-
+    drawOnCanvas (transformer : Transformer, canvas : HTMLCanvasElement) {
 
         let context = canvas.getContext ("2d");
 
-        window.console.log (translatedPoint.x);
-        window.console.log (translatedPoint.y);
-        window.console.log (translatedSize.width);
-        window.console.log (translatedSize.height);
-        
-        context.rect (translatedPoint.x, translatedPoint.y, 
-            translatedSize.width,  translatedSize.height);
+        context.beginPath();
+
+        let leftBottom  = transformer.translatePoint ({x: this.myStartPoint.x, y: this.myStartPoint.y});
+        let rightBottom = transformer.translatePoint ({x: this.myStartPoint.x + this.mySize.width, y: this.myStartPoint.y});
+
+        let rightTop = transformer.translatePoint ({x: this.myStartPoint.x + this.mySize.width, y: this.myStartPoint.y + this.mySize.height});
+        let leftTop  = transformer.translatePoint ({x: this.myStartPoint.x, y: this.myStartPoint.y + this.mySize.height});
+
+        context.moveTo(leftBottom.x, leftBottom.y);
+        context.lineTo(rightBottom.x, rightBottom.y);
+        context.lineTo(rightTop.x, rightTop.y);
+        context.lineTo(leftTop.x, leftTop.y);
+        context.closePath();
+        // context.fill();
 
         context.stroke();
     }
@@ -57,7 +61,6 @@ class Circle extends Shape {
 
         context.fillStyle = 'green';
         context.fill();
-        context.lineWidth = 5;
         context.strokeStyle = '#003300';
         context.stroke();
     }    
@@ -67,11 +70,12 @@ class Circle extends Shape {
 class Transformer {
     public canvasSize : Size = null
 
-    translateUpperLeft(p : Point, s : Size) : Point {
+    translatePoint (p: Point){
         let x = p.x * this.canvasSize.width;
-        let y = (1 - p.y - s.height) * this.canvasSize.height;
-        return {x: x, y: y};
-    } 
+        let y = (1 - p.y) * this.canvasSize.height;
+        return {x: x, y: y};        
+    }
+
 
     translateCenterPoint(p : Point, s : Size) : Point {
         let x = (p.x + 0.5 * s.width)* this.canvasSize.width;
@@ -90,9 +94,9 @@ class Transformer {
 
 
 class HorizontalMirrorTransformer extends Transformer{
-    translateUpperLeft(p : Point, s : Size) : Point {
-        let x = (1-p.x - s.width) * this.canvasSize.width;
-        let y = (1 - p.y - s.height) * this.canvasSize.height;
+    translatePoint(p : Point) : Point {
+        let x = (1-p.x) * this.canvasSize.width;
+        let y = (1 - p.y ) * this.canvasSize.height;
         return {x: x, y: y};
     } 
 
@@ -103,12 +107,44 @@ class HorizontalMirrorTransformer extends Transformer{
     } 
 }
 
+class RotateTransformer extends Transformer {
+    public angleToRotateInDeg = 10;
+    constructor (public angleInDeg=10) {
+        super();
+        this.angleToRotateInDeg = angleInDeg;
+    }
+
+    // Hoek uitrekenen tot boven scharnierpunt
+    // Lengte uitrekenen
+    // Nieuw punt uitrekenen aan de hand daarvan
+    translatePoint(p : Point) : Point {
+        let upperLeft : Point = {x: -0.01, y: 1.01}
+
+        let sideA = upperLeft.y - p.y;
+        let sideB = upperLeft.x + p.x ;
+        let sideC = Math.sqrt (sideA * sideA + sideB * sideB);
+
+        let differenceInRad = this.angleToRotateInDeg / 180 * Math.PI;
+        let angleInRad = Math.atan (sideA / sideB);
+        let angleToRotateInRad = angleInRad + differenceInRad;
+
+        let sideANew = upperLeft.x + Math.cos (angleToRotateInRad) * sideC;
+        let sideBNew = upperLeft.y - Math.sin (angleToRotateInRad) * sideC;
+        
+        window.console.log ({sideC:sideC, x:sideANew, y:sideBNew});
+        return super.translatePoint({x: sideANew, y: sideBNew});
+    } 
+
+
+}
+
+
 class DrawArea {
     private shapes : Shape[] = [];
     private myCanvas : HTMLCanvasElement = null;
     private myTransformer : Transformer; 
 
-    constructor (canvas : HTMLCanvasElement, transformer : Transformer = new Transformer()) {
+    constructor (canvas : HTMLCanvasElement, transformer : Transformer) {
        this.myCanvas = canvas;
        transformer.canvasSize = {width: canvas.width, height: canvas.height};
        
@@ -129,33 +165,35 @@ class DrawArea {
 
 
 
-let r1 = new Rectangle({x:0, y:0}, {width:1, height:1})
-let r2 = new Rectangle({x:0, y:0}, {width:0.5, height:0.5})
-
-let r3 = new Rectangle({x:0.75, y:0.75}, {width:0.1, height:0.1})
-
-let c1 = new Circle ({x:0.5, y:0.5}, {width:0.1, height:0.1});
+let r1 = new Rectangle({x:0.5, y:0.5}, {width:0.2, height:0.2})
+let r2 = new Rectangle({x:0.3, y:0.5}, {width:0.2, height:0.2})
+let r3 = new Rectangle({x:0.3, y:0.8}, {width:0.2, height:0.2})
+let r4 = new Rectangle({x:0.5, y:0.8}, {width:0.2, height:0.2})
+let r5 = new Rectangle({x:0.5, y:0.9}, {width:0.2, height:0.2})
+let c1 = new Circle({x:0.1, y:0.1}, {width:0.1, height:0.1});
 
 let canvas1 = document.getElementById("tekenvlak1")
-let canvas2 = document.getElementById("tekenvlak2")
 
-let drawArea1 = new DrawArea (<HTMLCanvasElement> canvas1);
-let drawArea2 = new DrawArea (<HTMLCanvasElement> canvas2, new HorizontalMirrorTransformer());
+let canvas2 = document.getElementById("tekenvlak2")
+let canvas3 = document.getElementById("tekenvlak3")
+
+let drawArea1 = new DrawArea (<HTMLCanvasElement> canvas1, new RotateTransformer());
+let drawArea2 = new DrawArea (<HTMLCanvasElement> canvas2, new Transformer());
+let drawArea3 = new DrawArea (<HTMLCanvasElement> canvas2, new RotateTransformer(-10));
 
 drawArea1.addShape(r1);
 drawArea1.addShape(r2);
 drawArea1.addShape(r3);
-
+drawArea1.addShape(r4);
+drawArea1.addShape(r5);
 drawArea1.addShape(c1);
-
-drawArea1.drawOnCanvas();
-
-// Mirror ding
 
 drawArea2.addShape(r1);
 drawArea2.addShape(r2);
-drawArea2.addShape(r3);
 
-drawArea2.addShape(c1);
+drawArea3.addShape(r1);
+drawArea3.addShape(r2);
+
+drawArea1.drawOnCanvas();
 drawArea2.drawOnCanvas();
-
+drawArea3.drawOnCanvas();
